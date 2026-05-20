6c4622f2c463c2eb66209d4d77c34ee0bb2fc063
SN-ICE-002 <tim.roger@univ.edu.vu>
2026-05-20 15:39:27 +1100

    Test initial

--- api.js
+++ api.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Here is the staging Branch: stagging In Progress......')
 });
 
+app.get('/API', (req, res) => {
+  res.send('NEW API')
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
